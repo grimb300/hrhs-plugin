eb0453a62e9e7a546036b81f0767b260fcdb5b73
Bob Grim <grimb300@hotmail.com>
2021-02-23 14:33:15 -0800

    start creating the search form

--- includes/template-hrhs-search-page.php
+++ includes/template-hrhs-search-page.php
@@ -9,6 +9,14 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 get_header();
 
+// This seems like a bad idea, but I don't know of an alternate way (yet)
+// The HRHS_Plugin class has the info about the post types and fields that can be searched
+$hrhs = new HRHS_Plugin();
+$searchable_post_types = $hrhs->get_search_types_fields();
+
+hrhs_debug( 'Searchable post types:' );
+hrhs_debug( $searchable_post_types );
+
 $description = get_the_archive_description();
 ?>
 
@@ -18,6 +26,21 @@ $description = get_the_archive_description();
 <header class="page-header alignwide">
   <h1 class="page-title">HRHS Database Search</h1>
 </header><!-- .page-header -->
+
+<form role="search" class="widget widget_search" action="#" method="post">
+  <label for="hrhs-search-needle">Search...</label>
+  <input type="text" name="hrhs-search[needle]" id="hrhs-search-needle">
+  <input type="submit" class="search-submit " value="Search">
+</form>
+
+<?php
+// Check to see if the plugin has been instantiated
+if ( ! empty( $hrhs ) ) {
+  echo '<p>HRHS_Plugin is instantiated</p>';
+} else {
+  echo '<p>HRHS_Plugin is not instantiated</p>';
+}
+?>
 
 <?php while ( have_posts() ) : ?>
         <?php the_post(); ?>
