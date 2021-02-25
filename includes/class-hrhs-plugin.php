11cc413c89228a95034999ac1915209d8b2fc702
Bob Grim <grimb300@hotmail.com>
2021-02-25 09:09:14 -0800

    add remaining known databases to post type def, add searchable property to field def

--- includes/class-hrhs-plugin.php
+++ includes/class-hrhs-plugin.php
@@ -27,34 +27,53 @@ class HRHS_Plugin {
         'singular_name' => 'Name Entry',
         'plural_name' => 'Name Entries',
         'fields' => array(
-          array(
-            'slug' => 'surname',
-            'label' => 'Surname',
-          ),
-          array(
-            'slug' => 'givenname',
-            'label' => 'Given Name',
-          ),
-          array(
-            'slug' => 'birth',
-            'label' => 'Date of Birth',
-          ),
-          array(
-            'slug' => 'death',
-            'label' => 'Date of Death',
-          ),
-          array(
-            'slug' => 'marriage',
-            'label' => 'Spouse',
-          ),
-          array(
-            'slug' => 'remarks',
-            'label' => 'Remarks',
-          ),
-          array(
-            'slug' => 'infoname',
-            'label' => 'Info Name',
-          ),
+          array( 'label' => 'Surname',       'slug' => 'surname',   'searchable' => 'all', ),
+          array( 'label' => 'Given Name',    'slug' => 'givenname', 'searchable' => 'member' ),
+          array( 'label' => 'Date of Birth', 'slug' => 'birth',     'searchable' => 'member' ),
+          array( 'label' => 'Date of Death', 'slug' => 'death',     'searchable' => 'member' ),
+          array( 'label' => 'Spouse',        'slug' => 'marriage',  'searchable' => 'member' ),
+          array( 'label' => 'Remarks',       'slug' => 'remarks',   'searchable' => 'member' ),
+          array( 'label' => 'Info Name',     'slug' => 'infoname',  'searchable' => 'member' ),
+          array( 'label' => 'ID',            'slug' => 'ID',        'searchable' => 'none' ),
+        ),
+      ),
+      'news_info' => array(
+        'slug' => 'news_entry',
+        'singular_name' => 'News Entry',
+        'plural_name' => 'News Entries',
+        'fields' => array(
+          array( 'label' => 'Year',            'slug' => 'year',  'searchable' => 'member', ),
+          array( 'label' => 'Newspaper',       'slug' => 'news',  'searchable' => 'member' ),
+          array( 'label' => 'Number of Pages', 'slug' => 'pages', 'searchable' => 'member' ),
+          array( 'label' => 'Sort Order',      'slug' => 'sort',  'searchable' => 'none' ),
+       ),
+      ),
+      'place_info' => array(
+        'slug' => 'place_entry',
+        'singular_name' => 'Place Entry',
+        'plural_name' => 'Place Entries',
+        'fields' => array(
+          array( 'label' => 'Place Name',  'slug' => 'placename',  'searchable' => 'member', ),
+          array( 'label' => 'Other Name',  'slug' => 'othername',  'searchable' => 'member' ),
+          array( 'label' => 'Designator',  'slug' => 'designator', 'searchable' => 'member' ),
+          array( 'label' => 'Location',    'slug' => 'location',   'searchable' => 'member' ),
+          array( 'label' => 'Remarks',     'slug' => 'remarks',    'searchable' => 'member' ),
+          array( 'label' => 'Info Source', 'slug' => 'infosource', 'searchable' => 'member' ),
+        ),
+      ),
+      'obit_info' => array(
+        'slug' => 'obit_entry',
+        'singular_name' => 'Obituary Entry',
+        'plural_name' => 'Obituary Entries',
+        'fields' => array(
+          array( 'label' => 'Surname',       'slug' => 'surname',      'searchable' => 'all', ),
+          array( 'label' => 'Given Name',    'slug' => 'givenname',    'searchable' => 'member' ),
+          array( 'label' => 'Date of Birth', 'slug' => 'birth',        'searchable' => 'member' ),
+          array( 'label' => 'Date of Death', 'slug' => 'death',        'searchable' => 'member' ),
+          array( 'label' => 'Parents',       'slug' => 'parents',      'searchable' => 'member' ),
+          array( 'label' => 'Spouse',        'slug' => 'marriage',     'searchable' => 'member' ),
+          array( 'label' => 'Age',           'slug' => 'age',          'searchable' => 'member' ),
+          array( 'label' => 'Obit Location', 'slug' => 'obitlocation', 'searchable' => 'member' ),
         ),
       ),
     );
